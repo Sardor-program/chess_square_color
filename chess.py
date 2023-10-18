154856535724772a56e467c1729f511082906d74
Sardor-program <sardorali378@gmail.com>
2023-10-18 18:25:41 +0500

    Create chess.py

---
+++ chess.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def chessSquareColor(column, row):
+    if column%2 == row%2:
+        print('white')
+    else:
+        print('black')
+    
+    
+    
+    
+    
